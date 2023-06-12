ca6b9908e7ccfa1ac27ea68bd88e60677e01c791
MichelMattheIS <michel.matthe@infosupport.com>
2023-06-12 23:38:11 +0200

    WORKING version of CombinationFinder.cs

--- SixLetterWords/Services/CombinationFinder.cs
+++ SixLetterWords/Services/CombinationFinder.cs
@@ -1,4 +1,5 @@
-﻿using SixLetterWords.Models;
+﻿using System.Text.RegularExpressions;
+using SixLetterWords.Models;
 
 namespace SixLetterWords.Services;
 
@@ -7,26 +8,32 @@ public class CombinationFinder : ICombinationFinder
     public IEnumerable<Combination> FindAllCombinations(IEnumerable<string> words, int combinationLength)
     {
         var sixLetters = words.Where(s => s.Length == combinationLength).ToList();
-        var lessThanSixLetters = words.Where(s => s.Length < combinationLength).OrderByDescending(x => x.Length).ToList();
+        var lessThanSixLetters = words.Where(s => s.Length < combinationLength).ToList();
         List<Combination> result = new List<Combination>();
         foreach (string six in sixLetters)
         {
             string tmp = six;
             Combination combination = new Combination(new List<string>());
-            foreach (string lessSix in lessThanSixLetters)
+            int i = 0;
+            while (i < lessThanSixLetters.Count)
             {
-                if (tmp.Contains(lessSix))
+                var lessSix = lessThanSixLetters[i];
+                if (tmp.StartsWith(lessSix))
                 {
-                    tmp = tmp.Replace(lessSix, "");
+                    tmp = tmp.Substring(lessSix.Length);
                     var index = Math.Min(six.IndexOf(lessSix), combination.Words.Count);
-                    
+                    // Console.WriteLine($"\nsix: {six} | lessSix: {lessSix} | tmp: {tmp} | index: {index}");
                     combination.Words.Insert(index, lessSix);
+                    
+                    if (string.IsNullOrEmpty(tmp))
+                    {
+                        result.Add(combination);
+                        break;
+                    }
+                    
+                    i = 0;
                 }
-                if (string.IsNullOrEmpty(tmp))
-                {
-                    result.Add(combination);
-                    break;
-                }
+                else i++;
             }
         }
 
